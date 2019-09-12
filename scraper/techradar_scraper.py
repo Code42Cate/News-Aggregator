5056ce1ced0f0e6abac6f2af5fdff0fa7e1e0f9e
Jonas Scholz <jonas.scholz@bbscholz.de>
2019-09-12 19:20:42 +0200

    Added techradar scraper

---
+++ scraper/techradar_scraper.py
@@ -0,0 +1,25 @@
+from scraper import SiteScraper
+import requests
+from bs4 import BeautifulSoup
+
+
+class TechRadarScraper(SiteScraper):
+
+    def scrape(self):
+        page = requests.get("https://www.techradar.com")
+        soup = BeautifulSoup(page.text, features="lxml")
+        headlines = [x["aria-label"]
+                     for x in soup.find_all("a", "article-link")]
+        links = [x["href"] for x in soup.find_all("a", "article-link")]
+        self.__update_articles(list(zip(links, headlines)))
+        return self
+
+    # This should eventually already kill duplicates
+    def __update_articles(self, articles):
+        self.articles = articles
+
+    def get_articles(self):
+        return super().get_articles()
+
+
+print(TechRadarScraper().scrape().get_articles())
