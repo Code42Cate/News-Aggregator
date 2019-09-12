ec421cc2a599d6326bcf8caea2bfe51d87486b43
Jonas Scholz <jonas.scholz@bbscholz.de>
2019-09-12 19:26:22 +0200

    Added thenextweb scraper

---
+++ scraper/thenextweb_scraper.py
@@ -0,0 +1,23 @@
+from scraper import SiteScraper
+import requests
+import xml.etree.ElementTree as ET
+
+
+class TheNextWebScraper(SiteScraper):
+
+    def scrape(self):
+        page = requests.get("https://thenextweb.com/feed/")
+        result_list = []
+        root = ET.fromstring(page.text)
+        for item in root.find("channel").findall("item"):
+            result_list.append(
+                (item.find("title").text, item.find("link").text))
+        self.__update_articles(result_list)
+        return self
+
+    # This should eventually already kill duplicates
+    def __update_articles(self, articles):
+        self.articles = articles
+
+    def get_articles(self):
+        return super().get_articles()
