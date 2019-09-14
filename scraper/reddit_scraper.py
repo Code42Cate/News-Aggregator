13a618cfeda94c26922a35b36562bb218359bcb2
Jonas Scholz <jonas.scholz@bbscholz.de>
2019-09-14 21:27:05 +0200

    Reddit scraper is now using asyncio to scrape the subreddits in parallel

--- scraper/reddit_scraper.py
+++ scraper/reddit_scraper.py
@@ -1,27 +1,35 @@
 from scraper import SiteScraper
 import requests
-from bs4 import BeautifulSoup
+from aiohttp import ClientSession
+import asyncio
 
 
 class RedditScraper(SiteScraper):
+
     def scrape(self):
         subreddit_list = ["https://www.reddit.com/r/programming", "https://www.reddit.com/r/technology",
                           "https://www.reddit.com/r/compsci", "https://www.reddit.com/r/netsec", "https://www.reddit.com/r/webdev"]
         result = []
+        tasks = []
+        loop = asyncio.get_event_loop()
         for subreddit in subreddit_list:
             url = subreddit + "/top.json"
-            # User-Agent is apparently super important on reddit or you get throttled hard
-            r = requests.get(url, headers={'User-agent': 'Chrome'})
-            listings = r.json()
-            for child in listings['data']['children']:
-                # Only want external links
-                if "https://www.reddit.com" not in child['data']['url']:
-                    result.append(
-                        (child['data']['url'], child['data']['title']))
+            task = asyncio.ensure_future(self.__async_scrape(url, result))
+            tasks.append(task)
+        loop.run_until_complete(asyncio.wait(tasks))
         self.__update_articles(result)
         return self
 
-    # This should eventually already kill duplicates
+    async def __async_scrape(self, url, result):
+        async with ClientSession(headers={"User-Agent": "Chrome"}) as session:
+            async with session.get(url) as response:
+                listings = await response.json()
+                for child in listings['data']['children']:
+                    # Only want external links
+                    if "https://www.reddit.com" not in child['data']['url']:
+                        result.append(
+                            (child['data']['url'], child['data']['title']))
+
     def __update_articles(self, articles):
         self.articles = articles
 
