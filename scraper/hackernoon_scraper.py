e6510dfe10b7fc7c5c1dfd97ee545ed4005a50b7
Jonas Scholz <jonas.scholz@bbscholz.de>
2019-09-14 21:38:41 +0200

    Hackernoon scraper is now using asyncio to scrape the seperate topics  in parallel

--- scraper/hackernoon_scraper.py
+++ scraper/hackernoon_scraper.py
@@ -1,22 +1,35 @@
 from scraper import SiteScraper
 from bs4 import BeautifulSoup
 import requests
+from aiohttp import ClientSession
+import asyncio
 
 
 class HackerNoonScraper(SiteScraper):
     def scrape(self):
         hackernoon_urls = ["https://hackernoon.com/tagged/cryptocurrency", "https://hackernoon.com/tagged/coding",
                            "https://hackernoon.com/tagged/artificial-intelligence", "https://hackernoon.com/tagged/futurism", "https://hackernoon.com/tagged/startups"]
+        result = []
+        tasks = []
+        loop = asyncio.get_event_loop()
         for hackernoon_topic_url in hackernoon_urls:
-            page = requests.get(hackernoon_topic_url)
-            soup = BeautifulSoup(page.text, features="lxml")
-            titles = soup.find_all("div", "title")
-            articles = []
-            for title in titles:
-                articles.append(("https://hackernoon.com" +
-                                 title.findChildren("a", recursive=False)[0]["href"], title.text))
-        self.__update_articles(articles)
+            task = asyncio.ensure_future(
+                self.__async_scrape(hackernoon_topic_url, result))
+            tasks.append(task)
+        loop.run_until_complete(asyncio.wait(tasks))
+        self.__update_articles(result)
         return self
+
+    async def __async_scrape(self, url, result_list):
+        async with ClientSession() as session:
+            async with session.get(url) as response:
+                text = await response.text(encoding="utf-8")
+                soup = BeautifulSoup(text, features="lxml")
+                titles = soup.find_all("div", "title")
+                articles = []
+                for title in titles:
+                    result_list.append(("https://hackernoon.com" +
+                                        title.findChildren("a", recursive=False)[0]["href"], title.text))
 
     # This should eventually already kill duplicates
     def __update_articles(self, articles):
