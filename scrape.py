9311fb55f9ef184b24f26bccc206901a19c06444
Jonas Scholz <jonas.scholz@bbscholz.de>
2019-09-14 21:26:29 +0200

    Add time measurement to scraping

--- scrape.py
+++ scrape.py
@@ -1,9 +1,10 @@
 import utils
 from scraper import SiteScraper
+import time
 
 
 def aggregate():
-
+    start = time.time()
     articles = []
     for Scraper in SiteScraper.__subclasses__():
         articles.extend(Scraper().scrape().get_articles())
@@ -11,9 +12,12 @@ def aggregate():
     for url, title in articles:
         print('"{}","{}"'.format(title.replace("\n", "").replace('"', "'"), url))
     articles = utils.remove_duplicates(articles)
+    #articles = utils.filter_by_keywords(articles, ["google", "security", "startup"])
     utils.articles_to_html(articles)
-    utils.articles_to_vocabulary(articles)
+    # utils.articles_to_vocabulary(articles)
     utils.articles_to_csv(articles)
+    end = time.time()
+    print("Scraping took: {} seconds".format(end - start))
 
 
 if __name__ == "__main__":
