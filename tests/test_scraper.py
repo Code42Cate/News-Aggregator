0a45e23ab7fa79b01c6f2769a72139eaeb05e0bf
Jonas Scholz <jonas.scholz@bbscholz.de>
2019-09-17 09:18:00 +0200

    Added unit tests to check for correct tuple size and correct URLs

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,6 +1,7 @@
 import unittest
 from scraper import SiteScraper
 import socket
+import re
 
 
 class TestAllScraperNoInternet(unittest.TestCase):
@@ -30,6 +31,34 @@ class TestAllScraperNoInternet(unittest.TestCase):
         for Scraper in SiteScraper.__subclasses__():
             articles.extend(Scraper().scrape().get_articles())
         self.assertEqual([], articles)
+
+
+class TestAllScraperOutput(unittest.TestCase):
+    def testTupleOutput(self):
+        # Basially our header, should always be the same format. Currently: ("url", "title")
+        tuple_size = len(SiteScraper().scrape().get_articles()[0])
+        for Scraper in SiteScraper.__subclasses__():
+            articles = Scraper().scrape().get_articles()
+            for article in articles:
+                self.assertEqual(tuple_size, len(article))
+
+    def testURLOutput(self):
+        for Scraper in SiteScraper.__subclasses__():
+            articles = Scraper().scrape().get_articles()
+            for url, title in articles:
+                self.assertTrue(self.is_valid_url(url))
+
+    """Weird somewhat okay-ish URL Validator. Mostly taken from django, so credit goes to them: https://github.com/django/django/blob/master/django/core/validators.py#L74"""
+
+    def is_valid_url(self, url):
+        regex = re.compile(
+            r'^https?://'  # http:// or https://
+            # domain...
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{1,63}\.?|'
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        return url is not None and regex.search(url)
 
 
 if __name__ == "__main__":
