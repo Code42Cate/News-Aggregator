febcc8cb2222fbd735435564ab949ce9eeb9b712
Jonas Scholz <jonas.scholz@bbscholz.de>
2019-09-18 02:26:36 +0200

    More logging, Article objects and less trash

--- scrape.py
+++ scrape.py
@@ -1,28 +1,41 @@
 import utils
 from scraper import SiteScraper
 import time
+from article import Article
 
 
 def aggregate():
-    start = time.time()
-    articles = []
 
+    start = time.time()
+    articles = []  # List of Article Tuples
+    article_objects = []  # List of Article Objects after removing duplicates
+
+    # Get all URL + Headline Tuples
     for Scraper in SiteScraper.__subclasses__():
         articles.extend(Scraper().scrape().get_articles())
-
-    for url, title in articles:
-        print('"{}","{}"'.format(title.replace("\n", "").replace('"', "'"), url))
-    articles = utils.add_timestamps(articles)
-    articles = utils.remove_duplicates(articles)
-    filtered_articles = utils.filter_by_keywords(
-        articles, ["google", "security", "startup", "programming"])
-    print(filtered_articles)
-    utils.articles_to_html(articles)
-    utils.articles_to_vocabulary(articles)
-    utils.articles_to_csv(articles)
-
     end = time.time()
     print("Scraping took: {} seconds".format(end - start))
+    start = time.time()
+    articles = utils.remove_duplicates(articles)
+    end = time.time()
+    print("Removing duplicates took: {} seconds".format(end - start))
+    start = time.time()
+    # Init all Article Objects
+    for url, title in articles:
+        article_objects.append(Article(url, title))
+    end = time.time()
+    print("Initializing objects took: {} seconds".format(end - start))
+    start = time.time()
+    for article in article_objects:
+        article.process()
+    end = time.time()
+    print("Processing articles took: {} seconds".format(end - start))
+    start = time.time()
+    utils.articles_to_html(article_objects)
+    end = time.time()
+    print("Filling template took: {} seconds".format(end - start))
+    # utils.articles_to_vocabulary(articles)
+    # utils.articles_to_csv(articles)
 
 
 if __name__ == "__main__":
