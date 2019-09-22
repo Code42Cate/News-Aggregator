ca9e10134e4fa47e8c9b2f0bfc700f88139b545e
Jonas Scholz <jonas.scholz@bbscholz.de>
2019-09-22 22:31:28 +0200

    Add function to query all articles from database

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
-
+from article import Article
 client = MongoClient()
 db = client.news_database
 articles_collection = db.articles
 
 
 def save_articles(articles):
+    insert_articles = []
     for article in articles:
         obj = article.to_beautiful_object()
         obj["title"] = obj["title"].encode(
@@ -15,5 +16,16 @@ def save_articles(articles):
         for i, keyword in enumerate(obj["keywords"]):
             obj["keywords"][i] = keyword.encode(
                 "utf-8", "ignore").decode("utf-8", "ignore")
-        article_id = articles_collection.insert_one(obj).inserted_id
-        print("Inserted article, ID: {}".format(article_id))
+        insert_articles.append(obj)
+
+    articles_collection.insert_many(insert_articles)
+    print("Inserted {} articles".format(len(articles)))
+
+
+def get_articles():
+    article_objects = []
+    all_articles = list(articles_collection.find({}))
+    for article in all_articles:
+        article_objects.append(Article(
+            article["url"], article["title"], content=article["content"], keywords=article["keywords"]))
+    return article_objects
